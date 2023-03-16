5890e3ef3b5501b49c305e23d14eb9c65c889062
Will Taylor <106984134+william-tay@users.noreply.github.com>
2023-03-16 12:38:16 -0500

    Add files via upload

--- main.py
+++ main.py
@@ -1,48 +1,32 @@
+import random
+
 from chess import Board, Move, STARTING_FEN, polyglot
 import chess
 
 board = chess.Board(chess.STARTING_FEN)
 
-def main():
+print(board)
 
+with chess.polyglot.open_reader("baron30.bin") as reader:
+    for entry in reader.find_all(board):
+        print(entry.move, entry.weight, entry.learn)
 
+board.push_san("Nh3")
 
+print(board)
 
-    with chess.polyglot.open_reader("baron30.bin") as reader:
-        for entry in reader.find_all(board):
-            print(entry.move, entry.weight, entry.learn)
+real = list(board.legal_moves)
 
-    while not board.is_checkmate():
-        print(board)
-        legalMoves = [board.legal_moves]
-        playerMove(legalMoves)
+print(real)
+listRand = real
 
+print(listRand)
+test = random.choice(listRand)
+test1 = str(test)
+print(type(test1))
 
-
-# print(board.legal_moves)
-
-# print(chess.Move.from_uci("g1f3") in board.legal_moves)
-# print(board)
-# board.san(chess.Move(chess.E2, chess.E4))
-# board.parse_san('Nf3')
-# print(board.variation_san([chess.Move.from_uci(m) for m in ["e2e4", "e7e5", "g1f3"]]))
-
-
-
-
-def playerMove(legal):
-    print(legal)
-    while True:
-        try:
-            move = input("What is your move: ")
-            board.push_san(move)
-            break
-        except:
-            print("illegal move")
-
-def computerMove():
-    pass
-
-
-if __name__ == "__main__":
-    main()
+print(board)
+#board.push_uci(test)
+board.push_uci(test1)
+print(board)
+print(f'Hey this is a {test1}')
