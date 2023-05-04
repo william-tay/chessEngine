b8f681ebc6e59ceed9b1e047fa39a6b1861a138f
Will Taylor <106984134+william-tay@users.noreply.github.com>
2023-05-04 10:22:00 -0500

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,6 @@ from chess import Board, Move, STARTING_FEN, polyglot
 import chess
 import random
 
-
-evals = 0
-
 piece_values = {
     chess.PAWN: 100,
     chess.ROOK: 500,
@@ -60,7 +57,7 @@ rook_heat_map = [
     0, 0, 0, 5, 5, 0, 0, 0
 ]
 
-king_heat_map = [
+king_black_heat_map = [
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30,
     -30, -40, -40, -50, -50, -40, -40, -30,
@@ -70,6 +67,8 @@ king_heat_map = [
     20, 20, 0, 0, 0, 0, 20, 20,
     20, 30, 10, 0, 0, 10, 30, 20
 ]
+
+king_white_heat_map = list(reversed(king_black_heat_map))
 
 king_endgame_heat_map = [
     -50, -40, -30, -20, -20, -30, -40, -50,
@@ -106,26 +105,46 @@ def main():
     # for entry in reader.find_all(board):
     # print(entry.move, entry.weight, entry.learn)
     while not board.is_checkmate():
+        # print(board.legal_moves.count())
         if board.turn:
             result = eval() * 1
-        elif not board.turn:
+            print(f'This is white eval: {result}')
+        else:
             result = eval() * -1
-        print(board.turn)
-        print(result)
-        print(f'{board}\n')
+            print(f'This is black eval: {result}')
         legalMoves = [board.legal_moves]
         playerMove(legalMoves)
+        print(f'{board}\n')
         if board.turn:
             result = eval() * 1
-        elif not board.turn:
+            print(f'This is white eval: {result}')
+        else:
             result = eval() * -1
-        print(board.turn)
-        print(result)
+            print(f'This is black eval: {result}')
         computerMove()
+        print(f'{board}\n')
 
 
-def alphaBeta(alpha, beta, leftbranch):
-    pass
+"""
+This is going to be the start of alphaBeta, however it is a little hard to implement because many of the evaluation logic
+is still missing, for example obvious blunders where pieces are being lost, currently only looks at heat map
+"""
+
+
+def alphaBeta(moves):
+    best = 0
+    print(board.turn)
+    for i in moves:
+        print(i)
+        current_board = eval()
+        print(i in board.legal_moves)
+        board.push_uci(str(i))
+        after_move = eval()
+        diff = current_board - after_move
+        if diff < best:
+            best = diff
+        print(diff)
+        board.pop()
 
 
 # print(board.legal_moves)
@@ -145,21 +164,21 @@ def playerMove(legal):
         except:
             print("illegal move")
 
+
 def computerMove():
     real = list(board.legal_moves)
+    alphaBeta(real)
 
-    print(real)
-    listRand = real
+    #listRand = real
 
-    test = random.choice(listRand)
+  #  test = random.choice(listRand)
 
-    test1 = str(test)
-    board.push_uci(test1)
-
+   # test1 = str(test)
+  #  board.push_uci(test1)
 
 
 def eval():
-    global evals
+    evals = 0
     for squares in chess.SQUARES:
         piece = board.piece_type_at(squares)
         checkPawn = chess.Piece(1, board.turn)
@@ -170,46 +189,33 @@ def eval():
         checkKing = chess.Piece(6, board.turn)
         check = chess.Piece(piece, board.color_at(squares))
 
-        print(squares)
-        print(board.piece_type_at(squares))
-
-        if checkPawn.color and checkPawn.piece_type == check.piece_type:
-            print(f'This is squaresPawn: {squares} {board.piece_type_at(squares)}')
+        if checkPawn.color and check.color and checkPawn.piece_type == check.piece_type:
             evals = evals + pawn_white_heat_map[squares]
-            print(pawn_white_heat_map[squares])
-        elif not checkPawn.color and checkPawn.piece_type == check.piece_type:
-            print(f'This is squaresPawn: {squares} {board.piece_type_at(squares)}')
+        elif not checkPawn.color and not check.color and checkPawn.piece_type == check.piece_type:
             evals = evals + pawn_black_heat_map[squares]
-        elif checkKnight.color == check.color and checkKnight.piece_type == check.piece_type:
-            print(f'This is squaresKnight: {squares} {board.piece_type_at(squares)}')
+
+        if checkKing.color and check.color and checkKing.piece_type == check.piece_type:
+            evals = evals + king_white_heat_map[squares]
+        elif not checkKing.color and not check.color and checkKing.piece_type == check.piece_type:
+            evals = evals + king_black_heat_map[squares]
+
+        if checkKnight.color == check.color and checkKnight.piece_type == check.piece_type:
             evals = evals + knight_heat_map[squares]
-            print(knight_heat_map[squares])
-        elif checkBishop.color == check.color and checkBishop.piece_type == check.piece_type:
-            print(f'This is squaresBishop: {squares} {board.piece_type_at(squares)}')
+
+        if checkBishop.color == check.color and checkBishop.piece_type == check.piece_type:
             evals = evals + bishop_heat_map[squares]
-            print(bishop_heat_map[squares])
-        elif checkRook.color == check.color and checkRook.piece_type == check.piece_type:
-            print(f'This is squaresRook: {squares} {board.piece_type_at(squares)}')
+
+        if checkRook.color == check.color and checkRook.piece_type == check.piece_type:
             evals = evals + rook_heat_map[squares]
-            print(rook_heat_map[squares])
-        elif checkQueen.color == check.color and checkQueen.piece_type == check.piece_type:
-            print(f'This is squaresQueen: {squares} {board.piece_type_at(squares)}')
+
+        if checkQueen.color == check.color and checkQueen.piece_type == check.piece_type:
             evals = evals + queen_heat_map[squares]
-            print(queen_heat_map[squares])
-        elif checkKing.color == check.color and checkKing.piece_type == check.piece_type:
-            print(f'This is squaresKing: {squares} {board.piece_type_at(squares)}')
-            evals = evals + king_heat_map[squares]
-            print(king_heat_map[squares])
-       # if checkKing.color == check.color and checkPawn.piece_type == check.piece_type:
-           # print(f'This is squaresKingLate: {squares} {board.piece_type_at(squares)}')
-           # evals = evals + king_endgame_heat_map[squares]
-
-    print(f'This is {evals}')
+    # if checkKing.color == check.color and checkPawn.piece_type == check.piece_type:
+    # print(f'This is squaresKingLate: {squares} {board.piece_type_at(squares)}')
+    # evals = evals + king_endgame_heat_map[squares]
+    material = checkMaterial()
+    evals = evals + material
     return evals
-
-
-
-
 
 
 def checkMaterial():
@@ -227,8 +233,10 @@ def checkMaterial():
             white_material += piece_values[piece.piece_type]
         elif piece.color == chess.BLACK:
             black_material += piece_values[piece.piece_type]
-    print(white_material)
-    print(black_material)
+    if board.turn:
+        return white_material
+    else:
+        return black_material
 
 
 if __name__ == "__main__":
